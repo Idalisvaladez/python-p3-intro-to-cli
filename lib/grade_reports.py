fbcd4d0cf3c81eb44486e67a6a2ef3831e280512
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-21 20:04:41 -0500

    Complete lab

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+def create_grade_reports(student_grades):
+    with open('reports/grade_report.txt', 'w') as grades:
+        for grade in student_grades:
+            grades.write(grade + '\n')
+
+
 if __name__ == '__main__':
-    pass
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        grade = input("Student name, grade: ")
+    create_grade_reports(student_grades)
